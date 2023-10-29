0ccc72775fdc46b24f3245a3f91202a7c63dee31
Yair-3 <yairkimmel@gmail.com>
2023-10-29 09:08:45 -0400

    Added Partner names in the EchoServer Doc

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
+
+// PROJECT PARTNERS MARK KIMMEL AND DOVI KIMMEL
 public class EchoServer {
     public static void main(String[] args) throws IOException {
 
